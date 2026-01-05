41c999cc27eb1bc3e3cabaefe8faf6a0391f4eaf
Administrator <Administrator@Jim>
2026-01-05 13:31:59 +0800

    init

---
+++ NetBeansProjects/Wifi/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=OE-Gnueabi_1-Windows
+CND_ARTIFACT_DIR_Release=dist/V1/App
+CND_ARTIFACT_NAME_Release=Wifi
+CND_ARTIFACT_PATH_Release=dist/V1/App/Wifi
+CND_PACKAGE_DIR_Release=dist/Release/OE-Gnueabi_1-Windows/package
+CND_PACKAGE_NAME_Release=wifi.tar
+CND_PACKAGE_PATH_Release=dist/Release/OE-Gnueabi_1-Windows/package/wifi.tar
+# Debug configuration
+CND_PLATFORM_Debug=OE-Gnueabi-Windows
+CND_ARTIFACT_DIR_Debug=dist/V1/Debug
+CND_ARTIFACT_NAME_Debug=Wifi
+CND_ARTIFACT_PATH_Debug=dist/V1/Debug/Wifi
+CND_PACKAGE_DIR_Debug=dist/Debug/OE-Gnueabi-Windows/package
+CND_PACKAGE_NAME_Debug=wifi.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/OE-Gnueabi-Windows/package/wifi.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
